0967bd777108cc10608b445e9f8fb47495b5db87
Robert Goetz <unknown@unknown.com>
2010-03-29 18:44:33 +0000

    Fixed: Python script for code generation.

--- Builds/GenFCs.py
+++ Builds/GenFCs.py
@@ -7,48 +7,10 @@ def convertPathToOutput(Path):
    if os.name == "posix":
         return Path
    else:
-       return '"' + Path + '"'
+	    return '"' + Path + '"'
 
 def GenFCsFromFile(GenFCsPyDir, FcdCompilePath, Path):
-   os.system(convertPathToOutput(FcdCompilePath + " -b -f " + convertPathToOutput(Path)))
-
-   BaseHeaderFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + "Base.h"
-   BaseInlineFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + "Base.inl"
-   FieldsFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + "Fields.h"
-   BaseCodeFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + "Base.cpp"
-
-   HeaderFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + ".h"
-   InlineFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + ".inl"
-   CodeFile = "KE" + os.path.splitext(os.path.split(Path)[1])[0] + ".cpp"
-
-   BaseDir = os.path.dirname(Path)
-   if(BaseDir == ""):
-       BaseDir = os.getcwd()
-
-   #Only move the files if the current working directory is not where they should be put
-   if(os.name != "posix" or (not os.path.samefile(os.getcwd(), BaseDir))):
-       shutil.move(BaseHeaderFile, os.path.join(os.path.split(Path)[0],BaseHeaderFile))
-       shutil.move(BaseInlineFile, os.path.join(os.path.split(Path)[0],BaseInlineFile))
-       shutil.move(FieldsFile, os.path.join(os.path.split(Path)[0],FieldsFile))
-       shutil.move(BaseCodeFile, os.path.join(os.path.split(Path)[0],BaseCodeFile))
-
-       if(not os.path.exists(os.path.join(os.path.split(Path)[0],HeaderFile))):
-           shutil.move(HeaderFile, os.path.join(os.path.split(Path)[0],HeaderFile))
-       else:
-          print(os.path.join(os.path.split(Path)[0],HeaderFile) + " already exists.")
-          os.remove(HeaderFile)
-
-       if(not os.path.exists(os.path.join(os.path.split(Path)[0],InlineFile))):
-           shutil.move(InlineFile, os.path.join(os.path.split(Path)[0],InlineFile))
-       else:
-          print(os.path.join(os.path.split(Path)[0],InlineFile) + " already exists.")
-          os.remove(InlineFile)
-
-       if(not os.path.exists(os.path.join(os.path.split(Path)[0],CodeFile))):
-           shutil.move(CodeFile, os.path.join(os.path.split(Path)[0],CodeFile))
-       else:
-          print(os.path.join(os.path.split(Path)[0],CodeFile) + " already exists.")
-          os.remove(CodeFile)
+   os.system(convertPathToOutput(FcdCompilePath + " -d " + convertPathToOutput(Path) + " -b -f " + " -p " + convertPathToOutput(os.path.split(Path)[0])))
 
 
 def main():
